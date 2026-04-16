048254519348a56ac560b9b7b4985f083a7f1995
Kenwin <kenwinshikaaap@gmail.com>
2026-04-16 05:55:07 +0000

    Set up FastAPI with CORS and routing

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from smart-banking!")
+from fastapi import FastAPI
+from src.api.v1.routes.query import router as query_router
+from src.api.v1.routes.upload import router as upload_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(query_router, prefix="/api/v1", tags=["Query"])
+app.include_router(upload_router, prefix="/api/v1", tags=["Upload"])
+
+@app.get("/")
+def root():
+    return {
+        "status":"MULTI MODEL RAG SYSTEM"
+    }
+
+@app.get("/health")
+def health_check():
+      return {
+        "status":"working fine"
+    }
+   
