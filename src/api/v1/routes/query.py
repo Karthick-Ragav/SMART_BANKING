73eeedee782cf2202b4da78458c0ab1ff97496f0
Karthick-Ragav <karthiraghav.2003@gmail.com>
2026-04-16 05:56:42 +0000

    query route completed

---
+++ src/api/v1/routes/query.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from src.api.v1.schemas.query_schema import QueryRequest, QueryResponse
+from src.api.v1.services.query_services import query_documents 
+
+router = APIRouter()
+
+@router.post("/query", response_model=QueryResponse)
+def query_endpoint(request: QueryRequest):
+    results= query_documents(request.query)
+    return results
